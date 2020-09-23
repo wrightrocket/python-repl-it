c73021b6bcfc6dcab9495e885e9728134d21247f
wrightrocket <wright.keith@gmail.com>
2020-09-22 19:51:39 -0700

    added triple quote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env py -3
 print('Welcome to the Python midterm review') # first print
 print('View at http://github.com/wrightrocket/python-repl-it')
-
+''' Another type of comment'''
 # start review
 
